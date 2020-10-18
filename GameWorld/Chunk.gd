399f8ca001f82d41e431f02a42087346de765ad6
Johnny Stene <jhonnystene@gmail.com>
2020-10-17 20:18:35 -0600

    Add crappy ground texture

--- GameWorld/Chunk.gd
+++ GameWorld/Chunk.gd
@@ -21,9 +21,11 @@ func generate(chunkX, noise):
 	name = str(chunkX)
 	surfaceTool.begin(Mesh.PRIMITIVE_LINES)
 	surfaceTool.add_color(Color(GlobalData.world_color))
+	#surfaceTool.set_material(load("res://Materials/GroundMaterialShader.tres"))
 	
 	surfaceToolBelow.begin(Mesh.PRIMITIVE_TRIANGLES)
 	surfaceToolBelow.add_color(Color(GlobalData.world_color))
+	#surfaceToolBelow.set_material(load("res://Materials/GroundMaterialShader.tres"))
 	
 	var genX = chunkX * GlobalData.chunkSize
 	var generatedMesh = Mesh.new()
@@ -55,13 +57,27 @@ func generate(chunkX, noise):
 		surfaceToolBelow.add_vertex(Vector3(genX + x + 1, noise.get_noise_2d(genX + x + 1, 1) * GlobalData.terrainMultiplier, 0))
 		surfaceToolBelow.add_uv(Vector2(genX + x, 10000))
 		surfaceToolBelow.add_vertex(Vector3(genX + x, 10000, 0))
-		
+
 		surfaceToolBelow.add_uv(Vector2(genX + x, noise.get_noise_2d(genX + x, 1) * GlobalData.terrainMultiplier))
 		surfaceToolBelow.add_vertex(Vector3(genX + x, noise.get_noise_2d(genX + x, 1) * GlobalData.terrainMultiplier, 0))
 		surfaceToolBelow.add_uv(Vector2(genX + x + 1, noise.get_noise_2d(genX + x + 1, 1) * GlobalData.terrainMultiplier))
 		surfaceToolBelow.add_vertex(Vector3(genX + x + 1, noise.get_noise_2d(genX + x + 1, 1) * GlobalData.terrainMultiplier, 0))
 		surfaceToolBelow.add_uv(Vector2(genX + x + 1, 10000))
 		surfaceToolBelow.add_vertex(Vector3(genX + x + 1, 10000, 0))
+
+#		surfaceToolBelow.add_uv(Vector2(0, noise.get_noise_2d(genX + x, 1) * GlobalData.terrainMultiplier))
+#		surfaceToolBelow.add_vertex(Vector3(genX + x, noise.get_noise_2d(genX + x, 1) * GlobalData.terrainMultiplier, 0))
+#		surfaceToolBelow.add_uv(Vector2(1, noise.get_noise_2d(genX + x + 1, 1) * GlobalData.terrainMultiplier))
+#		surfaceToolBelow.add_vertex(Vector3(genX + x + 1, noise.get_noise_2d(genX + x + 1, 1) * GlobalData.terrainMultiplier, 0))
+#		surfaceToolBelow.add_uv(Vector2(0, 10000))
+#		surfaceToolBelow.add_vertex(Vector3(genX + x, 10000, 0))
+#
+#		surfaceToolBelow.add_uv(Vector2(0, noise.get_noise_2d(genX + x, 1) * GlobalData.terrainMultiplier))
+#		surfaceToolBelow.add_vertex(Vector3(genX + x, noise.get_noise_2d(genX + x, 1) * GlobalData.terrainMultiplier, 0))
+#		surfaceToolBelow.add_uv(Vector2(1, noise.get_noise_2d(genX + x + 1, 1) * GlobalData.terrainMultiplier))
+#		surfaceToolBelow.add_vertex(Vector3(genX + x + 1, noise.get_noise_2d(genX + x + 1, 1) * GlobalData.terrainMultiplier, 0))
+#		surfaceToolBelow.add_uv(Vector2(1, 10000))
+#		surfaceToolBelow.add_vertex(Vector3(genX + x + 1, 10000, 0))
 		
 	points.append(Vector2(genX + GlobalData.chunkSize, 10000))
 	points.append(Vector2(genX, 10000))
@@ -69,9 +85,12 @@ func generate(chunkX, noise):
 	
 	surfaceToolBelow.commit(bottomMesh)
 	$FillerMesh.set_mesh(bottomMesh)
+	#$FillerMesh.material = load("res://Materials/GroundShaderMaterial.tres")
 	
 	surfaceTool.commit(generatedMesh)
 	$DisplayMesh.set_mesh(generatedMesh)
+	#$DisplayMesh.material = load("res://Materials/GroundShaderMaterial.tres")
+	
 	
 	var collisionShape
 	if(GlobalData.collisionType == GlobalData.COLLISION_TYPE_CONVEX):
